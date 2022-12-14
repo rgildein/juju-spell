3b9d9381bf9cd18867d6aeb09772f018c3506dc6
jneo8 <james.lin@canonical.com>
2022-12-14 10:55:42 +0800

    fix(multijuju/filter.py): Change the filter logic to AND instead of OR

--- multijuju/filter.py
+++ multijuju/filter.py
@@ -1,8 +1,9 @@
+"""Filter logic."""
 import re
 
 from multijuju.settings import CONFIG_PATH
 
-from .config import Config, load_config
+from .config import Config, Controller, load_config
 
 # --filter "a=v1,v2,v3 b=v4,v5,v6"
 FILTER_STR_REGEX = r"([^=]+)=([^=]+)(?:\s|$)"
@@ -11,15 +12,19 @@ FILTER_STR_REGEX = r"([^=]+)=([^=]+)(?:\s|$)"
 def make_controllers_filter(filter_str):
     """Build filter func to config's controller."""
 
-    def filter(controller):
+    def filter(controller: Controller):
         """Filter controllers."""
+        # If the filter_str is "a=v1,v2,v3 b=v4,v5,v6"
+        # This will iterate over keys [a,b] to check the value
+        # inside controller match the values list a in [v1,v2,v3]
+        # and b in [v4,v5,v6].
         for key, values in re.findall(
             FILTER_STR_REGEX,
             filter_str,
         ):
-            if controller.get(key) in values.split(","):
-                return True
-        return False
+            if not controller.get(key) in values.split(","):
+                return False
+        return True
 
     return filter
 
