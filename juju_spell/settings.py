4206a3eba97acaf8fc2ade4512580b4ec709809b
jneo8 <james.lin@canonical.com>
2023-01-18 16:41:41 +0800

    fix(juju_spell/settings.py): Use SNAP_REAL_NAME to get the real user's home directory

--- juju_spell/settings.py
+++ juju_spell/settings.py
@@ -5,7 +5,16 @@ import pathlib
 APP_NAME = "juju-spell"
 APP_VERSION = "0.0.1"
 JUJUSPELL_DATA = pathlib.Path(
-    os.environ.get("JUJUSPELL_DATA", pathlib.Path.home() / ".local/share/juju-spell")
+    os.environ.get(
+        "JUJUSPELL_DATA",
+        pathlib.Path(
+            # The $HOME will be /snap/juju-spell/current after snap packaging.
+            # We need to use $SNAP_REAL_HOME to get the real user's
+            # home directory in snap.
+            os.environ.get("SNAP_REAL_HOME", pathlib.Path.home())
+        )
+        / ".local/share/juju-spell",
+    )
 )
 
 CONFIG_PATH = os.environ.get(
